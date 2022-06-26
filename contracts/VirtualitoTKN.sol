3656d870d2559f56d70c376b5247b15e53db27aa
Luis Fernando Ushiña <guffenix@gmail.com>
2022-06-25 22:06:40 -0500

    changing 18 to 6 decimal places

--- contracts/VirtualitoTKN.sol
+++ contracts/VirtualitoTKN.sol
@@ -8,4 +8,8 @@ contract VirtualitoTKN is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         _mint(msg.sender, 100000 * (10**decimals()));
     }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
 }
